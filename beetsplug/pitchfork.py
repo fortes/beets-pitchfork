88532347f6b06e19ebe87d0add59fa114b46075c
Filipe Fortes <accounts@fortes.com>
2018-08-20 16:24:31 -0400

    Fetch rating on import

--- beetsplug/pitchfork.py
+++ beetsplug/pitchfork.py
@@ -6,6 +6,15 @@ from optparse import OptionParser
 from pitchfork import search
 
 class PitchforkPlugin(BeetsPlugin):
+    def __init__(self):
+        super(PitchforkPlugin, self).__init__()
+        self.config.add({
+            'auto': False,
+        })
+
+        if self.config['auto']:
+            self.register_listener('album_imported', self.on_import)
+
     @property
     def album_types(self):
         return  {
@@ -17,6 +26,9 @@ class PitchforkPlugin(BeetsPlugin):
 
     def commands(self):
         return [PitchforkCommand(self.config, self._log)]
+
+    def on_import(self, lib, album):
+        fetch_review(album, self._log, False)
 
 class PitchforkCommand(ui.Subcommand):
     def __init__(self, config, log):
@@ -41,26 +53,34 @@ class PitchforkCommand(ui.Subcommand):
     def batch_fetch_reviews(self, lib, albums, force):
         """Fetch review for each album"""
         for album in albums:
-            rating = album.get('pitchfork_score')
-            if rating != None and rating != '' and not force:
-                message = ui.colorize('text_highlight_minor', u'has rating %s' % rating)
-                self._log.info(u'{0}: {1}', album, message)
-                continue
+            fetch_review(album, self._log, force)
 
-            try:
-                self._log.debug(u'Querying {0.albumartist} - {0.album}', album)
-                review = search(album['albumartist'], album['album'])
-                score = float(review.score())
-            except IndexError as e:
-                self._log.debug(u'No review found: {0}', e)
-                message = ui.colorize('text_error', u'no review found')
-                self._log.info(u'{0}: {1}', album, message)
-                continue
+def fetch_review(album, log, force):
+    rating = album.get('pitchfork_score')
+    if rating != None and rating != '' and not force:
+        message = ui.colorize('text_highlight_minor', u'has rating %s' % rating)
+        log.info(u'{0}: {1}', album, message)
+        return
 
-            message = ui.colorize('text_success', u'found review %s' % review.score())
-            album['pitchfork_bnm'] = review.best_new_music()
-            album['pitchfork_description'] = review.abstract()
-            album['pitchfork_score'] = score
-            album['pitchfork_url'] = 'https://pitchfork.com%s' % review.url
-            album.store()
-            self._log.info(u'{0}: {1}', album, message)
+    try:
+        log.debug(u'Querying {0.albumartist} - {0.album}', album)
+        review = search(album['albumartist'], album['album'])
+        score = float(review.score())
+    except IndexError as e:
+        log.debug(u'No review found: {0}', e)
+        message = ui.colorize('text_error', u'no review found')
+        log.info(u'{0}: {1}', album, message)
+        return
+    except Error as e:
+        log.debug(u'Error trying to get review: {0}', e)
+        message = ui.colorize('text_error', u'could not fetch review')
+        log.info(u'{0}: {1}', album, message)
+        return
+
+    message = ui.colorize('text_success', u'found review %s' % review.score())
+    album['pitchfork_bnm'] = review.best_new_music()
+    album['pitchfork_description'] = review.abstract()
+    album['pitchfork_score'] = score
+    album['pitchfork_url'] = 'https://pitchfork.com%s' % review.url
+    album.store()
+    log.info(u'{0}: {1}', album, message)
